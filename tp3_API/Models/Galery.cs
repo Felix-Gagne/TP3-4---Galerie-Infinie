9fd5f7bfb5fc6ebb0496e24e7ab948b4b0d5dba2
Félix Gagné <2070094@cegepmontpetit.ca>
2023-03-28 15:13:09 -0400

    Ajout des modèle user et galerie

---
+++ tp3_API/Models/Galery.cs
@@ -0,0 +1,10 @@
+﻿namespace tp3_API.Models
+{
+    public class Galery
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsPublic { get; set; }
+        public string DefaultImage { get; set; }
+    }
+}
---
+++ tp3_API/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace tp3_API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
